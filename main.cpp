e11562c0c585382b7fa223a2bd96e83b658f4475
Jason Katz-Brown <jason@katzbrown.com>
2002-04-09 05:45:41 +0000

    add simple command line option
    svn path=/trunk/kdegames/kolf/; revision=148058

--- main.cpp
+++ main.cpp
@@ -1,37 +1,76 @@
+#include <qcstring.h>
+#include <qfile.h>
+
 #include <kapplication.h>
 #include <klocale.h>
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
+#include <kdebug.h>
+
+#include <iostream.h>
 
 #include "kolf.h"
 
 static const char *description =
-        I18N_NOOP("KDE Minigolf Game");
+I18N_NOOP("KDE Minigolf Game");
 
-static const char *version = "0.1";
+static const char *version = "1.0";
+
+static KCmdLineOptions options[] = 
+{
+	{ "course-info ", I18N_NOOP("Print course information and quit."), 0 },
+	{ 0, 0, 0 }
+};
 
 
 int main(int argc, char **argv)
 {
-  KAboutData aboutData( "kolf", I18N_NOOP("Kolf"),
-    version, description, KAboutData::License_GPL,
-    "(c) 2002, Jason Katz-Brown", "", "http://www.katzbrown.com/kolf/");
+	KAboutData aboutData( "kolf", I18N_NOOP("Kolf"), version, description, KAboutData::License_GPL, "(c) 2002, Jason Katz-Brown", "", "http://www.katzbrown.com/kolf/");
 
-  aboutData.addAuthor("Jason Katz-Brown", I18N_NOOP("Main author"), "jason@katzbrown.com");
-  aboutData.addAuthor("Niklas Knutsson", I18N_NOOP("Advanced putting mode"), 0);
-  aboutData.addAuthor("Daniel Matza-Brown", I18N_NOOP("Working wall-bouncing algorithm"), 0);
-  aboutData.addCredit("Rob Renaud", I18N_NOOP("Wall-bouncing help"), 0);
-  aboutData.addCredit("Aaron Segio", I18N_NOOP("Suggestions, bug reports"), 0);
+	aboutData.addAuthor("Jason Katz-Brown", I18N_NOOP("Main author"), "jason@katzbrown.com");
+	aboutData.addAuthor("Niklas Knutsson", I18N_NOOP("Advanced putting mode"), 0);
+	aboutData.addAuthor("Daniel Matza-Brown", I18N_NOOP("Working wall-bouncing algorithm"), 0);
+	aboutData.addCredit("Rob Renaud", I18N_NOOP("Wall-bouncing help"), 0);
+	aboutData.addCredit("Aaron Segio", I18N_NOOP("Suggestions, bug reports"), 0);
 
-  KCmdLineArgs::init(argc, argv, &aboutData);
+	KCmdLineArgs::init(argc, argv, &aboutData);
+	KCmdLineArgs::addCmdLineOptions(options);
 
-  QApplication::setColorSpec(QApplication::ManyColor);
-  KApplication a;
+	// I've actually added this for my web site uploaded courses display
+	KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+	if (args->isSet("course-info"))
+	{
+		KCmdLineArgs::enable_i18n();
 
-  Kolf *top = new Kolf;
-  a.setMainWidget(top);
-  top->show();
+		QString filename(QFile::decodeName(args->getOption("course-info")));
+		if (QFile::exists(filename))
+		{
+			CourseInfo info;
+			KolfGame::courseInfo(info, filename);
 
-  return a.exec();
+			cout << info.name.latin1()
+			     << " - " << i18n("By %1").arg(info.author).latin1()
+			     << " - " << i18n("%1 holes").arg(info.holes).latin1()
+			     << " - " << i18n("par %1").arg(info.par).latin1()
+			     << endl;
+
+			return 0;
+		}
+		else
+		{
+			KCmdLineArgs::usage(i18n("Course %1 does not exist.").arg(filename.latin1()));
+		}
+	}
+
+	QApplication::setColorSpec(QApplication::ManyColor);
+	KApplication a;
+
+	args->clear();
+
+	Kolf *top = new Kolf;
+	a.setMainWidget(top);
+	top->show();
+
+	return a.exec();
 }
 
