ea77195f120b44504b822d73e47e0b66f56a4e25
Jason Katz-Brown <jason@katzbrown.com>
2002-04-17 14:16:57 +0000

    add two missing files [vector.(h|cpp)]
    svn path=/trunk/kdegames/kolf/; revision=150046

---
+++ vector.cpp
@@ -0,0 +1,87 @@
+#include <math.h>
+
+#include "vector.h"
+
+// Creates a vector with between two points
+Vector::Vector(const QPoint &source, const QPoint &dest) {
+	_magnitude = sqrt(pow(source.x() - dest.x(), 2) + pow(source.y() - dest.y(), 2));
+	_direction = atan2(source.y() - dest.y(), source.x() - dest.x());
+}
+
+// Creates an empty Vector
+Vector::Vector() {
+	_magnitude = 0.0;
+	_direction = 0.0;
+}
+
+// Copy another Vector object
+Vector::Vector(const Vector& v) {
+	_magnitude = v._magnitude;
+	_direction = v._direction;
+}
+
+// Set the X component
+void Vector::setComponentX(double x) {
+	setComponents(x, componentY());
+}
+
+// Set the Y component
+void Vector::setComponentY(double y) {
+	setComponents(componentX(), y);
+}
+
+// Operations with another Vector performs vector math
+Vector Vector::operator+(const Vector& v) {
+	double x = componentX() + v.componentX();
+	double y = componentY() + v.componentY();
+
+	return Vector(sqrt((x * x) + (y * y)), atan2(y, x));	
+}
+
+Vector Vector::operator-(const Vector& v) {
+	double x = componentX() - v.componentX();
+	double y = componentY() - v.componentY();
+
+	return Vector(sqrt((x * x) + (y * y)), atan2(y, x));	
+}
+
+Vector& Vector::operator+=(const Vector& v) {
+	setComponents(componentX() + v.componentX(), componentY() + v.componentY());
+	return *this;
+}
+
+Vector& Vector::operator-=(const Vector& v) {
+	setComponents(componentX() - v.componentX(), componentY() - v.componentY());
+	return *this;
+}
+
+double Vector::operator*(const Vector& v) {
+	return ((componentX() * v.componentX()) + (componentY() * v.componentY()));
+}
+
+// Operations with a single double value affects the magnitude
+Vector& Vector::operator+= (double m) {
+	_magnitude += m;
+	return *this;
+}
+
+Vector& Vector::operator-= (double m) {
+	_magnitude -= m;
+	return *this;
+}
+
+Vector& Vector::operator*= (double m) {
+	_magnitude *= m;
+	return *this;
+}
+
+Vector& Vector::operator/= (double m) {
+	_magnitude /= m;
+	return *this;
+}
+
+// Sets both components at once (the only way to do it efficently)
+void Vector::setComponents(double x, double y) {
+	_direction = atan2(y, x);
+	_magnitude = sqrt((x * x) + (y * y));
+}
---
+++ vector.h
@@ -0,0 +1,66 @@
+#ifndef _VECTOR_H
+#define _VECTOR_H
+
+#include <qpoint.h>
+
+// This and vector.h by Ryan Cummings
+
+// Implements a vector in 2D
+class Vector {
+  public:
+	// Normal constructors
+	Vector(double magnitude, double direction) { _magnitude = magnitude; _direction = direction; }
+	Vector(const QPoint& source, const QPoint& dest);
+	Vector();
+
+	// Copy constructor
+	Vector(const Vector&);
+
+	// Accessors, sorta
+	double componentX() const { return (_magnitude * cos(_direction)); };
+	double componentY() const { return (_magnitude * sin(_direction)); };
+
+	// Sets individual components
+	// Wrappers around setComponents(double, double) - below
+	void setComponentX(double x);
+	void setComponentY(double y);
+
+	// Sets both components at once
+	void setComponents(double x, double y);
+
+	// Accessors
+	double magnitude() const { return _magnitude; }
+	double direction() const { return _direction; }
+	void setMagnitude(double m) { _magnitude = m; }
+	void setDirection(double d) { _direction = d; }
+
+	// Vector math
+	Vector operator+(const Vector&);
+	Vector operator-(const Vector&);
+
+	Vector& operator+=(const Vector&);
+	Vector& operator-=(const Vector&);
+
+	// Dot product
+	double operator*(const Vector&);
+
+	// Magnitude math
+	Vector operator+(double m) { return Vector(_magnitude + m, _direction); }
+	Vector operator-(double m) { return Vector(_magnitude - m, _direction); }
+	Vector operator*(double m) { return Vector(_magnitude * m, _direction); }
+	Vector operator/(double m) { return Vector(_magnitude / m, _direction); }
+
+	Vector& operator+=(double m);
+	Vector& operator-=(double m);
+	Vector& operator*=(double m);
+	Vector& operator/=(double m);
+
+	// Return the vector's equalivent on the unit circle
+	Vector unit() const { return Vector(1.0, _direction); }
+
+  protected:
+	double _magnitude;
+	double _direction;
+};
+
+#endif
